41bd96b00dfd254d9290f8798f183beccbc9a862
Patrick Emmerich <mail@patrick-emmerich.de>
2018-02-15 10:29:54 +0100

    refactor

--- transnet/plot.py
+++ transnet/plot.py
@@ -28,16 +28,16 @@ def plot_data():
     df = get_df_for_type()
     df = preprocess_df(df, date_from=date_from, date_upto=date_upto)
 
-    logger.info(df.info())
-    logger.info(df.head())
-
     df[[actual_value_mw, 'seasonal', 'trend', 'residuals']].plot(figsize=(13, 8), subplots=False)
     plt.title('Timeseries')
     plt.savefig('plot_ts.png')
 
+    # define (residuals) timeseries, which we will forecast
+    timeseries = df.loc[~df['residuals'].isnull(), 'residuals']
+
     plt.figure(figsize=(13, 8))
     plt.title('Autocorrelation')
-    lag_acf = get_acf(df.loc[~df['residuals'].isnull(), 'residuals'], nlags=14 * 24 * 4)
+    lag_acf = get_acf(timeseries, nlags=14 * 24 * 4)
     plt.axvline(x=1 * 24 * 4, linestyle='--', color='gray')
     plt.axvline(x=7 * 24 * 4, linestyle='--', color='gray')
     # plt.axhline(y=1.96 / np.sqrt(len(df.loc[~df['residuals'].isnull(), 'residuals'])), linestyle='--', color='gray')
@@ -46,15 +46,15 @@ def plot_data():
 
     plt.figure(figsize=(13, 8))
     plt.title('Partial Autocorrelation')
-    lag_pacf = get_pacf(df.loc[~df['residuals'].isnull(), 'residuals'], nlags=24 * 4)
+    lag_pacf = get_pacf(timeseries, nlags=24 * 4)
     # plt.axhline(y=1.96 / np.sqrt(len(df.loc[~df['residuals'].isnull(), 'residuals'])), linestyle='--', color='gray')
     plt.scatter(x=range(len(lag_pacf)), y=list(lag_pacf))
     plt.savefig('plot_pacf.png')
 
-    series_ = df.loc[~df['residuals'].isnull(), 'residuals']
-    series_starts = series_.index[0]
-    train = series_.loc[series_starts: date_upto - horizon]
-    holdout = series_.loc[date_upto - 2 * horizon: date_upto]
+    # predict
+    series_starts = timeseries.index[0]
+    train = timeseries.loc[series_starts: date_upto - horizon]
+    holdout = timeseries.loc[date_upto - 2 * horizon: date_upto]
     test_stationarity(train)
 
     prediction = get_forecast(train, p=2, d=0, q=0, horizon=24 * 4)
