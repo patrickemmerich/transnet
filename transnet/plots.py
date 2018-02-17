4def6cbed181386f8cc38b9aba448556908e223d
Patrick Emmerich <mail@patrick-emmerich.de>
2018-02-17 21:51:48 +0100

    add horiz significance level lines

--- transnet/plots.py
+++ transnet/plots.py
@@ -4,7 +4,7 @@ matplotlib.use('agg')
 import matplotlib.pyplot as plt
 
 from statsmodels.tsa.stattools import acf, pacf
-
+import numpy as np
 import logging
 
 logging.basicConfig(level=logging.INFO)
@@ -20,10 +20,11 @@ def plot_ts(df):
 def plot_acf(timeseries):
     plt.figure(figsize=(13, 8))
     plt.title('Autocorrelation')
-    lag_acf = acf(timeseries, nlags=14 * 24 * 4)
+    lag_acf = acf(timeseries, nlags=6 * 24 * 4)
     plt.axvline(x=1 * 24 * 4, linestyle='--', color='gray')
     plt.axvline(x=7 * 24 * 4, linestyle='--', color='gray')
-    # plt.axhline(y=1.96 / np.sqrt(len(df.loc[~df['residuals'].isnull(), 'residuals'])), linestyle='--', color='gray')
+    plt.axhline(y=1.96 / np.sqrt(len(timeseries)), linestyle='--', color='gray')
+    plt.axhline(y=-1.96 / np.sqrt(len(timeseries)), linestyle='--', color='gray')
     plt.scatter(x=range(len(lag_acf)), y=list(lag_acf))
     plt.savefig('plot_acf.png')
 
@@ -32,7 +33,9 @@ def plot_pacf(timeseries):
     plt.figure(figsize=(13, 8))
     plt.title('Partial Autocorrelation')
     lag_pacf = pacf(timeseries, nlags=24 * 4)
-    # plt.axhline(y=1.96 / np.sqrt(len(df.loc[~df['residuals'].isnull(), 'residuals'])), linestyle='--', color='gray')
+    plt.axhline(y=1.96 / np.sqrt(len(timeseries)), linestyle='--', color='gray')
+    plt.axhline(y=-1.96 / np.sqrt(len(timeseries)), linestyle='--', color='gray')
+    plt.axvline(x=2, linestyle='--', color='gray')
     plt.scatter(x=range(len(lag_pacf)), y=list(lag_pacf))
     plt.savefig('plot_pacf.png')
 
